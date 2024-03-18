976f8746472e565f9c06a2254cddc06b8e197d96
adityaweb3 <ap7596112@gmail.com>
2024-03-18 17:24:15 +0530

    backend mongoDB setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express') ;
 const app = express() ; 
+const dbConfig = require('./config/dbConfig');
+require('dotenv').config() ; 
 const port = process.env.PORT || 5000 ;
-
 app.listen(port , ()=> console.log(`Node.js Server started on port ${port}`));
