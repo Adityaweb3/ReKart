d27a7f6e6195b275b4c88ac0194c9ba5e313156b
adityaweb3 <ap7596112@gmail.com>
2024-03-19 13:37:11 +0530

    middleware

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -2,8 +2,7 @@ const router = require('express').Router() ;
 const User = require('../models/userModel');
 const bcrypt = require("bcryptjs") ;
 const jwt = require('jsonwebtoken') ;
-
-
+const authMiddleware = require('../middlewares/authMiddleware');
 //New User Registeration : 
 
 router.post('/register' , async(req , res)=>{
@@ -74,5 +73,26 @@ router.post('/login' , async(req , res)=>{
         
     }
 })
+
+router.get("/get-current-user" ,authMiddleware , async(req,res)=>{
+    try {
+        const user = await User.findById(req.body.userId) ; 
+        res.send({
+            success : true , 
+            message : "User Fetched SuccessFully" , 
+            data : user ,
+
+        });
+        
+    } catch (error) {
+        res.send({
+            success : false , 
+            message : error.message  , 
+        });
+    }
+});
+
+
+
 
 module.exports = router ;
