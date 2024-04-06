02656e32cea601f6a1f38a8af7384fb1034cb762
adityaweb3 <ap7596112@gmail.com>
2024-04-06 12:43:05 +0530

    added product page ui

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Spinner from "./components/Spinner";
 import { useSelector } from "react-redux";
 import Profile from "./pages/Profile";
 import Admin from "./pages/Admin";
+import ProductInfo from "./pages/Productinfo";
 function App() {
   const {loading} = useSelector(state=>state.loaders) ;
   return (
@@ -15,6 +16,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProtectedPage><Home /></ProtectedPage>} />
+        <Route path="/product/:id" element={<ProtectedPage><ProductInfo /></ProtectedPage>} />
         <Route path="/profile" element={<ProtectedPage><Profile /></ProtectedPage>} />
         <Route path="/admin" element={<ProtectedPage><Admin /></ProtectedPage>} />
         <Route path="/login" element={<Login />} />
--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -14,7 +14,7 @@ function Products() {
             const response =await GetProducts(null);
             dispatch(SetLoader(false)) ;
             if(response.success){
-                setProducts(response.products) ;
+                setProducts(response.data) ;
             }
             
         } catch (error) {
