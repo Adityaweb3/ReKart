168218f1a970cfacdd96b027fd0a10e45555ba5b
adityaweb3 <ap7596112@gmail.com>
2024-04-07 19:40:30 +0530

    added categories and ages filters ui

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,73 +1,101 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { GetProducts } from '../../apicalls/products';
-import { message } from 'antd';
-import { SetLoader } from '../../redux/loadersSlice';
-import Divider from '../../components/Divider';
-import { useNavigate } from 'react-router-dom';
-import Filters from './Filters';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { GetProducts } from "../../apicalls/products";
+import { Input, message } from "antd";
+import { SetLoader } from "../../redux/loadersSlice";
+import Divider from "../../components/Divider";
+import { useNavigate } from "react-router-dom";
+import Filters from "./Filters";
 
 function Home() {
-  const [showFilters , setShowFilters]=React.useState(true) ;
-  const [products , setProducts]= React.useState([]) ;
-  const [filters , setFilters] = React.useState({
-    status : 'approved' ,
-  })
-  const navigate = useNavigate() ;
-  const dispatch = useDispatch() ;
+  const [showFilters, setShowFilters] = React.useState(true);
+  const [products, setProducts] = React.useState([]);
+  const [filters, setFilters] = React.useState({
+    status: "approved",
+    category :[] ,
+    age : [] ,
+  });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const getData = async() =>{
+  const getData = async () => {
     try {
-      dispatch(SetLoader(true)) ;
-      const response = await GetProducts(filters) ;
-      dispatch(SetLoader(false)) ;
-      if(response.success){
-        setProducts(response.data) ;
+      dispatch(SetLoader(true));
+      const response = await GetProducts(filters);
+      dispatch(SetLoader(false));
+      if (response.success) {
+        setProducts(response.data);
       }
     } catch (error) {
-      dispatch(SetLoader(false)) ;
-      message.error(error.message) ;
+      dispatch(SetLoader(false));
+      message.error(error.message);
     }
-  }
+  };
 
-  React.useEffect(()=>{
-    getData() 
+  React.useEffect(() => {
+    getData();
+  }, []);
 
-  },[])
-  const {user} =useSelector((state)=>state.users) ;
+
+  useEffect(()=>{
+    console.log(filters) ;
+  } ,[filters])
+  const { user } = useSelector((state) => state.users);
   return (
-    <div className='flex gap-5'>
-      {showFilters && <Filters showFilters={showFilters} setShowFilters={setShowFilters} filters={filters} setFilters={setFilters}/>}
-      <div className='flex flex-col gap-5'>
-        <div className='flex gap-5 items-center'>
-        {!showFilters && <i className="ri-equalizer-line text-xl cursor-pointer"
-        onClick={()=>setShowFilters(!showFilters)}
-        ></i>}
-        <input type='text' placeholder='Search Product Here' className='border border-gray-300 rounded border-solid w-full p-2 h-14' />
+    <div className="flex gap-5">
+      {showFilters && (
+        <Filters
+          showFilters={showFilters}
+          setShowFilters={setShowFilters}
+          filters={filters}
+          setFilters={setFilters}
+        />
+      )}
+      <div className="flex flex-col gap-5">
+        <div className="flex gap-5">
+          {!showFilters && (
+            <i
+              className="ri-equalizer-line text-xl cursor-pointer"
+              onClick={() => setShowFilters(!showFilters)}
+            ></i>
+          )}
+          <input
+            type="text"
+            placeholder="Search Product Here"
+            className="border border-gray-300 rounded border-solid px-2 py-1 h-14"
+          />
         </div>
-      <div className={`
-      grid  gap-5 ${showFilters ? "grid-cols-4" : "grid-cols-5"}`}>
-      {products?.map((product) =>{
-        return (<div className='border border-gray-350 rounded border-solid flex flex-col gap-2 pb-2 cursor-pointer'
-        key = {product._id}
-        onClick={()=>navigate(`/product/${product._id}`)}
+        <div
+          className={`
+      grid  gap-5 ${showFilters ? "grid-cols-4" : "grid-cols-5"}`}
         >
-          <img src = {product.images[0]}
-          className='w-full h-52 p-2 rounded-md' alt="" />
-          <div className='px-2 flex flex-col'>
-            <h1 className='text-lg font-semibold '>{product.name}</h1>
-            <p className='text-sm'>{product.description}</p>
-            <Divider />
-            <span className='text-xl font-semibold text-green-700'>$ {product.price}</span>
-          </div>
+          {products?.map((product) => {
+            return (
+              <div
+                className="border border-gray-350 rounded border-solid flex flex-col gap-2 pb-2 cursor-pointer"
+                key={product._id}
+                onClick={() => navigate(`/product/${product._id}`)}
+              >
+                <img
+                  src={product.images[0]}
+                  className="w-full h-52 p-2 rounded-md"
+                  alt=""
+                />
+                <div className="px-2 flex flex-col">
+                  <h1 className="text-lg font-semibold ">{product.name}</h1>
+                  <p className="text-sm">{product.description}</p>
+                  <Divider />
+                  <span className="text-xl font-semibold text-green-700">
+                    $ {product.price}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        )
-      })}
       </div>
-      </div>
-
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
